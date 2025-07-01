d4e214d89aaaab4bf997928126707c24cd35c549
thanhcong1710 <thanhcong1710@gmail.com>
2025-07-01 14:22:03 +0700

    update

--- resources/js/coreui/views/operating/reserves/add.vue
+++ resources/js/coreui/views/operating/reserves/add.vue
@@ -685,7 +685,7 @@
             // if(u.session().user.role_id!='999999999'){
             //   this.data.temp.max_date = moment(enrolment.end_date_schedule).subtract(1, 'd').format('YYYY-MM-DD');
             // }else{
-              this.data.temp.max_date = moment(enrolment.end_date).subtract(1, 'd').format('YYYY-MM-DD');
+              this.data.temp.max_date = moment(enrolment.end_date_schedule).subtract(1, 'd').format('YYYY-MM-DD');
             // }
 
             let passed_session = 0;
