ecd335b3f5a29a45e66c8cd6961999d4df8099ca
thanhcong1710 <thanhcong1710@gmail.com>
2026-08-12 21:24:20 +0700

    update

--- app/Http/Controllers/LMSAPIController.php
+++ app/Http/Controllers/LMSAPIController.php
@@ -214,7 +214,7 @@ class LMSAPIController
             FROM classes AS cl
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
             WHERE cl.id=$class_id");
-        if (in_array($class_info->product_id, [1, 2, 3, 100])) {
+        if (in_array($class_info->product_id, [1, 2, 3, 100]) && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
@@ -264,7 +264,7 @@ class LMSAPIController
             FROM classes AS cl
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
             WHERE cl.id=$class_id");
-        if (in_array($class_info->product_id, [1, 2, 3, 100])) {
+        if (in_array($class_info->product_id, [1, 2, 3, 100])  && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
@@ -323,7 +323,7 @@ class LMSAPIController
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
                 LEFT JOIN branches AS b ON b.id=c.branch_id
             WHERE c.id=$contract_id");
-        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])) {
+        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])  && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
@@ -373,7 +373,7 @@ class LMSAPIController
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
                 LEFT JOIN branches AS b ON b.id=c.branch_id
             WHERE c.student_id=$student_id AND c.product_id IN(1,2,3,100) AND c.class_id IS NOT NULL ORDER BY c.count_recharge DESC,c.id DESC  LIMIT 1");
-        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])) {
+        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])  && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
