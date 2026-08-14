c1b87b79d607178fc5cb384e201c1f91a36aa6e4
thanhcong1710 <thanhcong1710@gmail.com>
2026-08-14 18:02:57 +0700

    update

--- app/Http/Controllers/LMSAPIController.php
+++ app/Http/Controllers/LMSAPIController.php
@@ -209,7 +209,7 @@ class LMSAPIController
             FROM classes AS cl
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
             WHERE cl.id=$class_id");
-        if (in_array($class_info->product_id, [1, 2, 3, 100])) {
+        if (in_array($class_info->product_id, [1, 2, 3, 100])  && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
@@ -259,7 +259,7 @@ class LMSAPIController
             FROM classes AS cl
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
             WHERE cl.id=$class_id");
-        if (in_array($class_info->product_id, [1, 2, 3, 100])) {
+        if (in_array($class_info->product_id, [1, 2, 3, 100])   && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
@@ -318,7 +318,7 @@ class LMSAPIController
                 LEFT JOIN teachers AS t ON t.user_id=cl.teacher_id
                 LEFT JOIN branches AS b ON b.id=c.branch_id
             WHERE c.id=$contract_id");
-        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])) {
+        if ($student_info && in_array($student_info->product_id, [1, 2, 3, 100])   && 1==2) {
             $token = self::getTokenLMS();
             if (data_get($token, 'status') == 'SUCCESS') {
                 $token = $token->result->accessToken;
